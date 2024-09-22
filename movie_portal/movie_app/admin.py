933f0d0a684fd4483d31b2174dd4354e19ef465c
Godwin0397 <64906665+Godwin0397@users.noreply.github.com>
2024-09-22 22:06:48 +0530

    Add files via upload

---
+++ movie_portal/movie_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from movie_app.models import CineProfessionls, Movie, MovieReview
+
+# Register your models here.
+
+
+admin.site.register(CineProfessionls)
+admin.site.register(Movie)
+admin.site.register(MovieReview)
---
+++ movie_portal/movie_app/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class CineProfessionls(models.Model):
+    
+    """
+    To store information about Cine professionals
+    """
+
+    name = models.CharField(max_length=200, null=True)
+    profile = models.TextField(null=True)
+    date_of_birth = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+
+    """
+    To store information about movies
+    """
+
+    title = models.CharField(max_length=200)
+    plot = models.TextField()
+    cast = models.ManyToManyField(CineProfessionls, related_name="movie_cast")
+    producer = models.ForeignKey(CineProfessionls, related_name="movie_producer", on_delete=models.CASCADE)
+    director = models.ForeignKey(CineProfessionls, related_name="movie_director", on_delete=models.CASCADE)
+
+
+class MovieReview(models.Model):
+
+    """
+    To store information about movies review
+    """
+    
+    movie = models.ForeignKey(Movie, related_name="movie_review", on_delete=models.CASCADE)
+    review = models.TextField()
+
+
+
+
+
+
+
+
+
+
+
